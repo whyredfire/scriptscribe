35e2a356817bb810d267839c75e9eee8a524fc73
Karan Parashar <karan.parashar@samey.ai>
2025-08-01 02:20:36 +0530

    feat(backend): Use gunicorn for serving backend

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
-
 from src.routes.auth import auth_bp
 from src.routes.core import core_bp
 
-app = Flask(__name__)
-CORS(app, supports_credentials=True)
-
-
-@app.before_request
-def startup():
+def on_startup():
     import nltk
-
     nltk.download("punkt")
     nltk.download("punkt_tab")
     nltk.download("stopwords")
     nltk.download("wordnet")
 
+on_startup()
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(core_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
